95eb1c9dac1afbf516a33fbe75ae61cdddd9cf3c
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-03-08 12:06:11 -0400

    make some bits header-only includes

--- src/read-nmea.cpp
+++ src/read-nmea.cpp
@@ -6,189 +6,20 @@ namespace writable = cpp11::writable;
 #include <vector>
 #include <sstream>
 
-class Source {
-public:
-    virtual ~Source() {}
-    virtual size_t read(unsigned char* dst, size_t n_bytes) {
-        return 0;
-    }
-};
-
-class SourceRaw: public Source {
-public:
-    SourceRaw(raws src): src(src), data(RAW(src.data())), offset(0), size(src.size()) {}
-
-    size_t read(unsigned char* dst, size_t n_bytes) {
-        check_user_interrupt();
-        R_xlen_t new_offset = offset + n_bytes;
-        if (new_offset >= size) {
-            new_offset = size;
-        }
-
-        size_t n_cpy = new_offset - offset;
-        if (n_cpy > 0) {
-            memcpy(dst, data + offset, n_cpy);
-        }
-        this->offset = new_offset;
-
-        return n_cpy;
-    }
-
-private:
-    raws src;
-    unsigned char* data;
-    R_xlen_t offset;
-    R_xlen_t size;
-};
-
-class SourceConnection: public Source {
-public:
-    SourceConnection(sexp con): con(con) {}
-
-    size_t read(unsigned char* dst, size_t n_bytes) {
-        auto readBin = cpp11::package("base")["readBin"];
-        raws vals(readBin(con, "raw", n_bytes));
-        if (vals.size() > 0) {
-            memcpy(dst, RAW(vals), vals.size());
-        }
-
-        return vals.size();
-    }
-
-private:
-    sexp con;
-};
+#include "nmea/source.hpp"
 
 std::unique_ptr<Source> nmea_get_source(list src) {
     sexp obj = src["obj"];
 
     if (Rf_inherits(src, "nmea_src_raw")) {
-        return std::unique_ptr<Source>(new SourceRaw((raws) obj));
+        raws raw = (raws) obj;
+        return std::unique_ptr<Source>(new SourceBytes(RAW(raw), raw.size()));
     } else if (Rf_inherits(src, "nmea_src_connection")) {
         return std::unique_ptr<Source>(new SourceConnection(obj));
     } else {
         stop("Source must be a raw vector");
     }
 }
-
-class NMEAScanner {
-public:
-  NMEAScanner(Source* src, size_t buffer_size):
-    src(src), 
-    current_buffer_size(0), 
-    buffer_size(buffer_size), 
-    offset(0) {
-      this->str = (char*) malloc(buffer_size);
-    }
-
-    ~NMEAScanner() {
-        free(this->str);
-    }
-
-    std::string read_until(const std::string& needle, size_t max_size) {
-        size_t needle_size = needle.size();
-        if ((needle_size == 0) || (needle_size > 255)) {
-            stop("needle size must be between 1 and 255");
-        }
-
-        size_t max_read = max_size - needle_size;
-        char buffer[256];
-        memset(buffer, 0, 256);
-        std::stringstream out;
-
-        for (size_t n_read = 0; n_read <= max_read; n_read++) {
-            size_t n_read_buf = peek_n(buffer, needle_size);
-
-            if ((needle == buffer) || (n_read_buf != needle_size) || (n_read == max_read)) {
-                out << std::string(buffer, n_read_buf);
-                this->offset += n_read_buf;
-                break;
-            } else {
-                out << buffer[0];
-                this->offset++;
-            }
-        }
-
-        return out.str();
-    }
-
-    size_t skip_until(std::string& needle) {
-        if (this->finished()) {
-            return 0;
-        }
-
-        char buffer = '\0';
-        size_t n_skip = 0;
-        do {
-            peek_n(&buffer, 1);
-            for (size_t i = 0; i < needle.size(); i++) {
-                if (buffer == needle[i]) {
-                    return n_skip;
-                }
-            }
-            n_skip++;
-        } while(this->advance());
-       
-        return n_skip;
-    }
-
-    size_t peek_n(char* dest, size_t n) {
-        if ((this->offset + n) > this->current_buffer_size) {
-            size_t n_read = this->get_more_from_source(this->current_buffer_size - this->offset);
-            if (n_read < n) {
-                n = n_read;
-            }
-        }
-
-        if (n > 0) {
-            memcpy(dest, this->str + this->offset, n);
-        }
-
-        return n;
-    }
-
-    inline size_t get_more_from_source(size_t keeping = 0) {
-        if (keeping > 0) {
-            memmove(this->str, this->str + offset, keeping);
-        }
-
-        this->current_buffer_size = src->read(
-            (unsigned char*) this->str + keeping, 
-            this->buffer_size - keeping
-        );
-        this->offset = 0;
-        return this->current_buffer_size;
-    }
-
-    inline bool advance() {
-        if (!this->finished()) {
-            this->offset++;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    inline bool finished() {
-        if (this->offset < this->current_buffer_size) {
-            return false;
-        } else {
-            return this->get_more_from_source() == 0;
-        }
-    }
-
-private:
-    // a Source from which to read more data
-    Source* src;
-    // the current buffer
-    char* str;
-    // total number of valid characters in the current buffer
-    size_t current_buffer_size;
-    // total writable size of the buffer
-    size_t buffer_size;
-    // position of the cursor within the buffer
-    size_t offset;
-};
 
 [[cpp11::register]]
 list cpp_read_nmea(list obj,
@@ -197,10 +28,13 @@ list cpp_read_nmea(list obj,
     std::unique_ptr<Source> src = nmea_get_source(obj);
 
     // 65536 is what readr:::read_connection() uses by default
-    NMEAScanner scanner(src.get(), 65536);
+    Scanner scanner(src.get(), 65536);
 
     writable::list sentences;
     writable::integers offsets;
+
+    // without these sentences and offsets will be null
+    // in the case of zero sentences found
     sentences.reserve(10);
     offsets.reserve(10);
 
