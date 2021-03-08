3b32cf647da8dae64a2fb6f0ebff9d62781c9ff7
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-03-08 10:37:25 -0400

    test embedded null output

--- tests/testthat/test-read-nmea.R
+++ tests/testthat/test-read-nmea.R
@@ -6,29 +6,22 @@ test_that("read_nmea() can use filenames, raw vectors, or connections", {
   close(file_con)
 
   expect_identical(read_nmea(file), read_nmea(file_raw))
-
 })
 
 test_that("read_nmea() regenerates nmea_test_basic", {
   file <- system.file("extdata/basic.nmea", package = "nmea")
-  file_con <- file(file, open = "rb")
-  test_raw <- readBin(file_con, raw(), n = file.size(file))
-  close(file_con)
 
   expect_identical(
-    nmea_checksum(read_nmea(test_raw)$sentence),
+    nmea_checksum(read_nmea(file)$sentence),
     nmea_checksum(nmea_test_basic)
   )
 })
 
 test_that("read_nmea() max_length works", {
   file <- system.file("extdata/basic.nmea", package = "nmea")
-  file_con <- file(file, open = "rb")
-  test_raw <- readBin(file_con, raw(), n = file.size(file))
-  close(file_con)
 
-  abbrev <- read_nmea(test_raw, max_length = 10)
-  full_size <- read_nmea(test_raw)
+  abbrev <- read_nmea(file, max_length = 10)
+  full_size <- read_nmea(file)
 
   expect_identical(abbrev$offset, full_size$offset)
   expect_true(all(nmea_length(abbrev$sentence) == 10))
@@ -38,5 +31,18 @@ test_that("read_nmea() can read an empty file", {
   expect_identical(
     read_nmea(raw()),
     tibble::tibble(offset = integer(), sentence = nmea())
+  )
+})
+
+test_that("read_nmea() can read files with embedded nulls", {
+  # this file is an insult to the NMEA standard, containing raw serial
+  # output with embedded null characters and output longer than 82 bytes
+  file_nulls <- system.file("extdata/nulls.odc", package = "nmea")
+
+  odc <- read_nmea(file_nulls, sentence_end = "\r\n", max_length = 1024)
+  expect_identical(nrow(odc), 1000L)
+  expect_identical(
+    odc$offset,
+    c(0L, cumsum(nmea_length(odc$sentence))[-nrow(odc)])
   )
 })
