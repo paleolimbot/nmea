4c2dd9ba6c42fcf3658ad8fa97cbc54d1f2da41c
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-03-09 09:32:03 -0400

    more tests for checksum

--- tests/testthat/test-nmea-checksum.R
+++ tests/testthat/test-nmea-checksum.R
@@ -8,3 +8,55 @@ test_that("nmea_parse_checksum() works", {
   is_rmc <- nmea_message_type(nmea_test_basic) %in% c("RMC", "GGA")
   expect_identical(chk$calc[!is_rmc], chk$found[!is_rmc])
 })
+
+test_that("nmea_parse_checksum() works on invalid inputs", {
+  expect_identical(
+    nmea_parse_checksum(NA_character_),
+    tibble::tibble(
+      calc = NA_integer_,
+      found = NA_integer_,
+      start = NA_integer_,
+      end = NA_integer_
+    )
+  )
+
+  expect_identical(
+    nmea_parse_checksum(""),
+    tibble::tibble(
+      calc = 0L,
+      found = NA_integer_,
+      start = NA_integer_,
+      end = NA_integer_
+    )
+  )
+
+  expect_identical(
+    nmea_parse_checksum("$"),
+    tibble::tibble(
+      calc = 0L,
+      found = NA_integer_,
+      start = 1L,
+      end = NA_integer_
+    )
+  )
+
+  expect_identical(
+    nmea_parse_checksum("*"),
+    tibble::tibble(
+      calc = 0L,
+      found = NA_integer_,
+      start = NA_integer_,
+      end = 0L
+    )
+  )
+
+  expect_identical(
+    nmea_parse_checksum("*F-"),
+    tibble::tibble(
+      calc = 0L,
+      found = NA_integer_,
+      start = NA_integer_,
+      end = 0L
+    )
+  )
+})
